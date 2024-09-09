9fc205693386a1d53534192061cb7cf3040ca1bd
Kehad <keahnney01@gmail.com>
2024-09-09 04:06:32 +0100

    commit 5

--- src/components/works/works.jsx
+++ src/components/works/works.jsx
@@ -21,7 +21,50 @@ function Works() {
             </div>
           </div>
         </div>
-        <div></div>
+        <div className="items-container">
+          <div className="items-box">
+            <div className="items-1">
+              <p>1</p>
+            </div>
+            <div className="items-text">
+              <p className="items-text1">Lorem ipsum</p>
+              <p className="items-text2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+                consectetur adipiscing{" "}
+              </p>
+            </div>
+          </div>
+          <div className="items-box">
+            <div className="items-1">
+              <p>1</p>
+            </div>
+            <div className="items-text">
+              <p className="items-text1">Lorem ipsum</p>
+              <p className="items-text2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+                consectetur adipiscing{" "}
+              </p>
+            </div>
+          </div>
+          <div className="items-box">
+            <div className="items-1">
+              <p>1</p>
+            </div>
+            <div className="items-text">
+              <p className="items-text1">Lorem ipsum</p>
+              <p className="items-text2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+                consectetur adipiscing{" "}
+              </p>
+            </div>
+                  </div>
+                  <div className="button-box">
+                      
+                  <a href="#" className="booking">Make a booking</a>
+                  </div>
+
+                  
+        </div>
       </div>
     </div>
   );
