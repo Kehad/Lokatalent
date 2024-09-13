106001542663915eedb3286f8991f0039ea14c22
Kehad <keahnney01@gmail.com>
2024-09-13 03:38:25 +0100

    commit loca 28

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,40 +1,78 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+const activeClassName = "selected navlink";
+const activeStyleCallback = ({ isActive }) =>
+  isActive ? activeClassName : "navlink";
 
 const NavLinks = () => {
   return (
     <>
       <ul className="flex items-center flex-col sm:flex-col md:flex-row lg:flex-row list-none gap-[35px]">
         <li className="">
-          <Link
+          <NavLink
             to="/login"
-            className="hover:underline underline-offset-8 transition transition-all transition ease-in-out delay-150"
+            // className="hover:underline underline-offset-8 transition transition-all transition ease-in-out delay-150"
+            className={({ isActive }) =>
+              isActive
+                ? "underline underline-bold decoration-2 hover:underline underline-offset-8 transition ease-in-out delay-150"
+                : "hover:underline underline-offset-8 transition ease-in-out delay-150"
+            }
           >
             Log In
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link
+          <NavLink
             to="/signup"
-            className="hover:underline underline-offset-8 transition transition-all transition ease-in-out delay-150"
+            // className="hover:underline underline-offset-8 transition transition-all transition ease-in-out delay-150"
+            className={({ isActive }) =>
+              isActive
+                ? "underline underline-bold decoration-2 hover:underline underline-offset-8 transition ease-in-out delay-150"
+                : "hover:underline underline-offset-8 transition ease-in-out delay-150"
+            }
           >
             Sign Up
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link
+          <NavLink
             to="/register"
             className="px-4 py-[10px] border-white border-2 rounded "
           >
             Register as a talent
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </>
   );
 };
 
+const variants = {
+  hidden: {
+    opacity: 0,
+    y: -20, // Starts slightly above the original position
+  },
+  show: {
+    opacity: 1,
+    y: 0, // Moves to the original position
+    transition: {
+      type: "spring",
+      stiffness: 100,
+      damping: 15,
+    },
+  },
+  exit: {
+    opacity: 0,
+    y: -20, // Moves up when exiting
+    transition: {
+      duration: 0.3,
+    },
+  },
+};
 
 function Nav() {
   const [isOpen, setIsOpen] = useState(false);
@@ -54,7 +92,7 @@ function Nav() {
   // px-[7rem]
   return (
     <div className="bg-[#2659BF] px-[2rem] sm:px-[3rem] md:px-[3rem] lg:px-[5rem] py-8 text-white">
-      <div className="flex   flex-wrap justify-between items-center">
+      <div className="flex flex-wrap justify-between items-center">
         <p>Lokatalent</p>
         <div>
           <nav className="hidden sm:hidden md:block lg:block">
@@ -63,13 +101,73 @@ function Nav() {
         </div>
         {/*  To open side Navigation butto for mobile view */}
         <div className="block sm:block md:hidden lg:hidden">
-          <MenuRoundedIcon
-            className="text-white cursor-pointer"
-            style={{ width: "30px", height: "40px" }}
-            onClick={toggleNavbar}
-          />
+          {!isOpen && (
+            <MenuRoundedIcon
+              className="text-white cursor-pointer"
+              style={{ width: "30px", height: "40px" }}
+              onClick={toggleNavbar}
+            />
+          )}
+          {isOpen && (
+            <CloseRoundedIcon
+              className="text-white cursor-pointer"
+              style={{ width: "30px", height: "40px" }}
+              onClick={toggleNavbar}
+            />
+          )}
         </div>
-        <div className="basis-full">{isOpen && <NavLinks />}</div>
+        <AnimatePresence mode="wait">
+          <div className="basis-full">
+            {isOpen && (
+              <motion.div
+                layout="position"
+                key="nav-links"
+                variants={variants}
+                initial="hidden"
+                animate="show"
+                exit="exit"
+                className="mt-4 basis-full md:hidden"
+              >
+                <ul className="flex items-center flex-col sm:flex-col md:flex-row lg:flex-row list-none gap-[35px]">
+                  <li className="">
+                    <NavLink
+                      to="/login"
+                      // className="hover:underline underline-offset-8 transition transition-all transition ease-in-out delay-150"
+                      className={({ isActive }) =>
+                        isActive
+                          ? "text-red-500 hover:underline underline-offset-8 transition ease-in-out delay-150"
+                          : "hover:underline underline-offset-8 transition ease-in-out delay-150"
+                      }
+                    >
+                      Log In
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/signup"
+                      // className="hover:underline underline-offset-8 transition transition-all transition ease-in-out delay-150"
+                      className={({ isActive }) =>
+                        isActive
+                          ? "text-red-500 hover:underline underline-offset-8 transition ease-in-out delay-150"
+                          : "hover:underline underline-offset-8 transition ease-in-out delay-150"
+                      }
+                    >
+                      Sign Up
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/register"
+                      className="px-4 py-[10px] border-white border-2 rounded "
+                    >
+                      Register as a talent
+                    </NavLink>
+                  </li>
+                </ul>
+              </motion.div>
+            )}
+          </div>
+        </AnimatePresence>
       </div>
     </div>
   );
